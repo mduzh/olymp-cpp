f52d5fe604c1a9a13f2a55eefd5ba92bf6744dc4
Maxim Duzh <duzh.maxim@gmail.com>
2025-07-20 11:50:20 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main(){
-  cout << "Hello World!" << endl;
+  
+  int arr[] = {4, 5, 143, 345, 78, 1, 999};
 
+  int min = arr[0];
+  int max = arr[0];
+  for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+
+    if (arr[i] < min)
+       min = arr[i];
+
+    if (arr[i] > max)
+       max = arr[i];
+ }
+  
+  cout << "Min value: " << min << endl << "Max value: " << max << endl;
   return 0;
 }
